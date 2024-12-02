ce16d3b88190463b2936ba7bc47788951412a618
helenajr <helenajrobinson@gmail.com>
2024-12-02 14:43:04 +0000

    APP: added content to home page, added more info page

--- app/launch.py
+++ app/launch.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 pg = st.navigation([
         st.Page("home_page.py", title="Welcome!", icon=":material/add_circle:"),
-        st.Page("app.py", title="Run Simulation", icon=":material/laptop:")
+        st.Page("app.py", title="Run Simulation", icon=":material/laptop:"),
+        st.Page("more_info.py", title="More Information", icon=":material/book:")
      ])
 
 pg.run()
